53f476f1b4e2dc04014d849cb73f9b9a72b77e8c
lutvek <ludvig.aaberg@gmail.com>
2016-02-18 14:56:59 +0100

    added back buttons for Detailed and AddSensor views and set the title of those correctly

--- app/src/main/java/com/example/ludvig/sens/MainActivity.java
+++ app/src/main/java/com/example/ludvig/sens/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ludvig.sens;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -40,6 +41,11 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+    }
+
+    public void addSensor(View view) {
+        Intent intent = new Intent(this, AddSensor.class);
+        startActivity(intent);
     }
 
     /**************************** MENU OPTIONS *********************************/
