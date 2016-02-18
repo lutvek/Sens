a24b4846b45e680b8b0a2a63f74e79db11c04c22
andreashahr <andreas.hahr@gmail.com>
2016-02-18 17:25:04 +0100

    AddSensor - layout, spinner items

--- app/src/main/java/com/example/ludvig/sens/AddSensor.java
+++ app/src/main/java/com/example/ludvig/sens/AddSensor.java
@@ -2,6 +2,8 @@ package com.example.ludvig.sens;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddSensor extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class AddSensor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_sensor);
+
+        Spinner spinner = (Spinner) findViewById(R.id.sensor_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.sensors_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_selectable_list_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
