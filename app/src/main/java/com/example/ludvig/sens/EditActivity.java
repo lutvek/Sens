33aff77c16aa6d930c216217c283209ce9f78e71
hbogaeus <henry.bogaeus@gmail.com>
2016-02-22 12:15:55 +0100

    Fixed the EditActivity layout

--- app/src/main/java/com/example/ludvig/sens/EditActivity.java
+++ app/src/main/java/com/example/ludvig/sens/EditActivity.java
@@ -1,10 +1,13 @@
 package com.example.ludvig.sens;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -22,6 +25,47 @@ public class EditActivity extends AppCompatActivity {
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
+        // FIXME Temp values of fields
+        EditText enterName= (EditText) findViewById(R.id.newSensorName);
+        enterName.setText("Spis");
+
+        EditText enterID= (EditText) findViewById(R.id.newSensorID);
+        enterID.setText("2d0028000e47343432313031");
+
+        EditText maxTempText = (EditText) findViewById(R.id.max_temp);
+        maxTempText.setText("23");
+
+        EditText minTempText = (EditText) findViewById(R.id.min_temp);
+        minTempText.setText("19");
+
+        addFonts();
+    }
+
+    private void addFonts() {
+        TextView tv = (TextView) findViewById(R.id.enter_name);
+        Typeface faceLight = Typeface.createFromAsset(getAssets(),
+                "fonts/Raleway-Light.ttf");
+        tv.setTypeface(faceLight);
+        tv = (TextView) findViewById(R.id.enter_id);
+        tv.setTypeface(faceLight);
+        tv = (TextView) findViewById(R.id.sensor_type);
+        tv.setTypeface(faceLight);
+        tv = (TextView) findViewById(R.id.max_temp_text);
+        tv.setTypeface(faceLight);
+        tv = (TextView) findViewById(R.id.min_temp_text);
+        tv.setTypeface(faceLight);
+        tv = (TextView) findViewById(R.id.allow_alarm_text);
+        tv.setTypeface(faceLight);
+
+        Typeface faceRegular = Typeface.createFromAsset(getAssets(),
+                "fonts/Raleway-Regular.ttf");
+
+        tv = (TextView) findViewById(R.id.accept_button);
+        tv.setTypeface(faceRegular);
+        tv = (TextView) findViewById(R.id.cancel_button);
+        tv.setTypeface(faceRegular);
+        tv = (TextView) findViewById(R.id.delete_button);
+        tv.setTypeface(faceRegular);
     }
 
     public void addNewSensor(View view) {
